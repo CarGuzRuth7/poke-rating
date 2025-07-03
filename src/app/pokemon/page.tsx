212984a62baacec4dfd89ac7998f3257f3f3a3c4
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2025-07-03 19:55:25 -0300

    initial commit, List of kanto pokemon

---
+++ src/app/pokemon/page.tsx
@@ -0,0 +1,15 @@
+export default function PokemonDetails(){
+    return(
+        <main>
+
+        <div>
+            <h2>Pokemon Name</h2>
+            <p>type</p>
+
+        </div>
+        <div>
+            <p>Rate Pokemon</p>
+        </div>
+        </main>
+    )
+}
